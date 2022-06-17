a5740be8f7e4425f0cfeb2849c4ea6d8e89123d8
DaCurse <dacurse0@gmail.com>
2022-06-17 21:22:32 +0300

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,16 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-struct WordlistIterator {
-  reader: BufReader<File>,
-}
-
-impl WordlistIterator {
-  fn new(file: File) -> Self {
-    let reader = BufReader::new(file);
-    WordlistIterator { reader }
-  }
-}
-
-impl Iterator for WordlistIterator {
-  type Item = String;
-
-  fn next(&mut self) -> Option<Self::Item> {
-    let mut line = String::new();
-    match self.reader.read_line(&mut line) {
-      Ok(0) => None,
-      Err(_) => None,
-      _ => {
-        if line.starts_with("#") {
-          self.next()
-        } else {
-          Some(line.trim_end().to_owned())
-        }
-      }
-    }
-  }
+fn read_wordlist(file: File) -> impl Iterator<Item = String> {
+  BufReader::new(file)
+    .lines()
+    .map(Result::unwrap)
+    .filter(|line| !line.starts_with("#"))
 }
 
 fn main() {
   let file = File::open("./wordlist.txt").unwrap();
-  let wordlist = WordlistIterator::new(file);
-  for line in wordlist {
+  for line in read_wordlist(file) {
     println!("{}", line);
   }
 }
